59cdf53a3169fe6eac2d20c833a97c2dee4297bd
Walter Leibbrandt <walter@translate.org.za>
2008-06-19 14:05:58 +0000

    Implemented work-flow code in gui.EditArea. It works, although it can still easily be broken.
    When a word (surface form) is selected in the word list, the edit area is
    notified and loads the associated root and part of speech if applicable (the
    surface will usually not have any such association). The focus is given to
    the root selection combo box. After a root is selected or a new one entered,
    the focus moves to the part of speech combo box. This will usually not change
    and will usually not be allowed to change. If a new root was entered, the
    "Add Root" button is shown and must be clicked before the loaded surface form
    can be classified. Once "Add Root" is clicked the language database is updated
    with the changes and the "Add Root" button is replaced by the "OK"
    (classification) button. If an existing root was selected, but with a different
    part of speech, both the "Add Root" and "Modify Root" buttons will be shown.
    In this case "Add Root" will create a duplicate root with a different part of
    speech. "Modify Root" will update the existing root's associated part of speech.
    The "OK" button, like "Reject" and "Ignore", updates the current surface form's
    status and refreshes the word list (which causes the edit area to load the next
    word).
    
    The current implementation relies heavily on the ComboBoxEntryies' Entries'
    "activate" event (comboboxentry.child.connect("activate", ...)) for updating
    the GUI and can be broken by using tab or the mouse. At least there's now
    something to work with.
    
    svn path=/src/trunk/spelt/; revision=7561

--- spelt/gui/edit_area.py
+++ spelt/gui/edit_area.py
@@ -20,7 +20,7 @@
 
 import gobject, gtk, gtk.glade
 
-from common   import exceptions, _
+from common   import Configuration, exceptions, _
 from models   import LanguageDB, PartOfSpeech, Root, SurfaceForm
 from wordlist import WordList
 
@@ -30,7 +30,6 @@ class EditArea(object):
     """
     This class represents the editing area on the GUI.
     """
-
     # CONSTRUCTOR #
     def __init__(self, glade_xml, wordlist, langdb=None, gui=None):
         """Constructor.
@@ -40,42 +39,135 @@ class EditArea(object):
         assert isinstance(glade_xml, gtk.glade.XML)
         assert isinstance(wordlist, WordList)
 
+        self.config     = Configuration()
         self.current_sf = None
         self.glade_xml  = glade_xml
         self.gui        = gui
         self.langdb     = langdb
         self.wordlist   = wordlist
+
         self.wordlist.word_selected_handlers.append(self.on_surface_form_selected)
         self.__init_widgets()
 
     # METHODS #
+    def check_pos_text(self, text=None):
+        """If text was entered into cmb_pos, handle that text."""
+        if text is None:
+            text = self.cmb_pos.child.get_text()
+
+        if not text:
+            self.select_pos(None)
+            self.cmb_pos.grab_focus()
+            return
+
+        shortcut, name = ('|' in text) and ( [substr.strip() for substr in text.split('|')] ) or ('_', text)
+        # Search for POS on value and shortcut
+        pos = self.langdb.find(section='parts_of_speech', name=name, shortcut=shortcut)
+
+        if pos and len(pos) > 0:
+            # NOTE: If more than one part of speech matches, we use the first one
+            self.select_pos(pos[0])
+
+            if pos[0].id != self.current_root.pos_id:
+                self.set_sensitive(btn_add_root=True, btn_mod_root=True)
+                self.set_visible(btn_ok=False, btn_add_root=True, btn_mod_root=True)
+        else:
+            # If we get here, we have a new part of speech
+            self.select_pos(None)
+            # Only create a new part of speech if the config says we are allowed:
+            if not self.config.options['may_create_pos']:
+                self.cmb_pos.grab_focus()
+                return
+            self.cmb_pos.child.set_text(text)
+            self.current_pos = PartOfSpeech(name=name, shortcut=shortcut)
+
+            # Determine which buttons should be shown/active
+            if self.cmb_root.get_active() < 0:
+                # If no existing root is selected, we have a new root and btn_mod_root should be hidden
+                self.set_sensitive(btn_ok=False, btn_add_root=True, btn_mod_root=False)
+                self.set_visible(btn_ok=False, btn_mod_root=False)
+            else:
+                # Otherwise, only the part of speech have change; show btn_{add,mod}_root
+                self.set_sensitive(btn_ok=False, btn_add_root=True, btn_mod_root=True)
+                self.set_visible(btn_ok=False, btn_add_root=True, btn_mod_root=True)
+
+        # Select the focus to the first button in the list that is both visible and active
+        for btn in (self.btn_ok, self.btn_add_root, self.btn_mod_root):
+            if btn.get_property('visible') and btn.get_property('sensitive'):
+                btn.grab_focus()
+
+    def check_root_text(self, text=None):
+        """If text was entered into cmb_root, handle that text."""
+        if text is None:
+            text = self.cmb_root.child.get_text()
+
+        if not text:
+            self.select_root(None)
+            return
+
+        # First check if the text in entry is that of an existing root:
+        roots = self.langdb.find(section='roots', value=text)
+
+        if roots and len(roots) > 0:
+            # NOTE: If more than one root matches, we use the first one
+            self.select_root(roots[0])
+        else:
+            # If we get here, we have a new root on our hands
+            self.select_root(None) # Deselect root
+            self.cmb_root.child.set_text(text)
+            self.current_root = Root(value=text, user_id=self.config.options['user_id'])
+            self.select_pos(None)  # Deselect POS
+
+            self.set_visible(btn_ok=False, btn_add_root=True)
+            self.set_sensitive(cmb_pos=True, btn_add_root=False)
+
+        self.cmb_pos.grab_focus()
+
     def on_surface_form_selected(self, sf):
         """A proxied event handler for when a surface form is selected in the
             word list.
 
             See the documentation of spelt.gui.WordList.word_selected_handlers for
             the use of this method."""
+        if sf is None:
+            self.lbl_word.set_markup('')
+            self.select_root(None)
+            self.set_sensitive(btn_reject=False, btn_ignore=False)
+            self.set_visible(btn_ok=False)
+            return
+
         self.current_sf = sf
         self.lbl_word.set_markup('<b>%s</b>' % sf.value)
 
         if self.langdb is None:
             return
 
-        if not sf.root_id:
-            self.cmb_root.child.set_text('')
-            self.cmb_pos.child.set_text('')
-            self.cmb_root.set_active(-1)
-            self.cmb_pos.set_active(-1)
-            return
+        # Set GUI to its initial state
+        self.set_sensitive(
+            btn_reject=True,
+            btn_ignore=True,
+            btn_ok=True,
+            btn_add_root=True,
+            btn_mod_root=True,
+            cmb_pos=True
+        )
+        self.set_visible(btn_ok=True, btn_add_root=False, btn_mod_root=False)
 
-        roots_found = self.langdb.find(id=sf.root_id, section='roots')
-        # The roots_found list can have a maximum of 1 element, because we
-        # search the database on ID's. ID's are guaranteed to be unique by the
-        # models (via it's inheritence of IDModel).
-        if roots_found:
-            self.select_root(roots_found[0])
+        if not sf.root_id:
+            # sf does not have an associated root, so there's nothing to select
+            # in the combo boxes.
+            self.select_root(None)
         else:
-            raise exceptions.RootError(_( 'No root object found with ID %d' % (sf.root_id) ))
+            roots_found = self.langdb.find(id=sf.root_id, section='roots')
+            # The roots_found list can have a maximum of 1 element, because we
+            # search the database on ID's. ID's are guaranteed to be unique by the
+            # models (via it's inheritence of IDModel).
+            if roots_found:
+                self.select_root(roots_found[0])
+            else:
+                raise exceptions.RootError(_( 'No root object found with ID %d' % (sf.root_id) ))
+
+        self.cmb_root.grab_focus()
 
     def pos_tostring(self, pos):
         """How a PartOfSpeech object should be represented as a string in the GUI.
@@ -119,12 +211,21 @@ class EditArea(object):
 
             @type  root: models.Root
             @param root: The root to select in the combo box."""
+        if root is None:
+            # Deselect root
+            self.cmb_root.set_active(-1)
+            self.cmb_root.child.set_text('')
+            self.select_pos(None) # This deselects the part of speech too.
+            self.set_sensitive(cmb_pos=False)
+            return
         assert isinstance(root, Root)
         iter = self.root_store.get_iter_root()
 
         while self.root_store.iter_is_valid(iter):
             if self.root_store.get_value(iter, COL_MODEL) == root:
                 self.cmb_root.set_active_iter(iter)
+                self.current_root = root
+                self.set_sensitive(cmb_pos=True)
 
                 pos_found = self.langdb.find(id=root.pos_id, section='parts_of_speech')
                 # The pos_found list can have a maximum of 1 element, because we
@@ -144,15 +245,37 @@ class EditArea(object):
 
             @type  pos: models.PartOfSpeech
             @param pos: The part of speech to select in the combo box."""
+        if pos is None:
+            # Deselect POS
+            self.cmb_pos.set_active(-1)
+            self.cmb_pos.child.set_text('')
+            return
         assert isinstance(pos, PartOfSpeech)
         iter = self.pos_store.get_iter_root()
 
         while self.pos_store.iter_is_valid(iter):
             if self.pos_store.get_value(iter, COL_MODEL) == pos:
                 self.cmb_pos.set_active_iter(iter)
+                self.current_pos = pos
                 break
 
             iter = self.pos_store.iter_next(iter)
+
+    def set_sensitive(self, **kwargs):
+        """Set widgets' sensitivity based on keyword arguments.
+
+            Example: "set_sensitive(cmb_pos=False) disables cmb_pos."""
+        for widget, sensitive in kwargs.items():
+            if hasattr(self, widget):
+                getattr(self, widget).set_sensitive(sensitive)
+
+    def set_visible(self, **kwargs):
+        """Set widgets' visibility based on keyword arguments.
+
+            Example: "set_visible(btn_ok=False)" will hide btn_ok."""
+        for widget, visible in kwargs.items():
+            if hasattr(self, widget):
+                getattr(self, widget).set_property('visible', visible)
 
     def __init_widgets(self):
         """Get and initialize widgets from the Glade object."""
@@ -225,23 +348,45 @@ class EditArea(object):
         self.btn_ok.connect('clicked', self.__on_btn_ok_clicked)
         self.btn_reject.connect('clicked', self.__on_btn_reject_clicked)
         # Combo's
-        self.cmb_pos.connect('changed', self.__on_cmb_pos_changed)
-        self.cmb_root.connect('changed', self.__on_cmb_root_changed)
+        self.cmb_pos.connect('changed', self.__on_cmb_changed, self.select_pos)
+        self.cmb_root.connect('changed', self.__on_cmb_changed, self.select_root)
+        # ComboBoxEntry's Entry
+        self.cmb_pos.child.connect('activate', self.__on_entry_activated, self.check_pos_text)
+        self.cmb_root.child.connect('activate', self.__on_entry_activated, self.check_root_text)
         # EntryCompletions
-        self.pos_completion.connect('match-selected', self.__on_match_selected, self.cmb_pos)
-        self.root_completion.connect('match-selected', self.__on_match_selected, self.cmb_root)
+        self.pos_completion.connect('match-selected', self.__on_match_selected, self.cmb_pos, self.select_pos)
+        self.root_completion.connect('match-selected', self.__on_match_selected, self.cmb_root, self.select_root)
 
     def __match_pos(self, completion, key, iter):
         model = self.pos_store.get_value(iter, COL_MODEL)
+        if model is None:
+            return False
         return model.shortcut.lower().startswith(key) or model.name.lower().startswith(key)
 
     def __match_root(self, completion, key, iter):
         model = self.root_store.get_value(iter, COL_MODEL)
+        if model is None:
+            return False
         return model.value.startswith(key)
 
     # GUI SIGNAL HANDLERS #
     def __on_btn_add_root_clicked(self, btn):
-        pass
+        """Add the current root to the language database."""
+        if self.langdb.find(id=self.current_root.id, section='roots'):
+            # The root already exists, so we have to duplicate it. It should have a different POS, though.
+            self.current_root = Root(value=self.current_root.value, user_id=self.config.options['user_id'])
+        self.current_root.pos_id = self.current_pos.id
+        self.langdb.add_root(self.current_root)
+
+        if self.cmb_pos.get_active() < 0:
+            self.langdb.add_part_of_speech(self.current_pos)
+
+        self.refresh()
+
+        # Update GUI
+        self.set_sensitive(btn_ok=True, btn_add_root=False, btn_mod_root=False)
+        self.set_visible(  btn_ok=True, btn_add_root=False, btn_mod_root=False)
+        self.btn_ok.grab_focus()
 
     def __on_btn_ignore_clicked(self, btn):
         """Set the currently selected surface form's status to "ignored"."""
@@ -252,10 +397,32 @@ class EditArea(object):
         self.gui.reload_database()
 
     def __on_btn_mod_root_clicked(self, btn):
-        pass
+        """Update the current root with the selected part of speech."""
+        if self.cmb_pos.get_active() < 0:
+            self.langdb.add_part_of_speech(self.current_pos)
+
+        self.current_root.user_id = self.config.options['user_id']
+        self.current_root.pos_id  = self.current_pos.id
+
+        self.refresh()
+
+        # Update GUI
+        self.set_sensitive(btn_ok=True, btn_add_root=False, btn_mod_root=False)
+        self.set_visible(  btn_ok=True, btn_add_root=False, btn_mod_root=False)
+        self.btn_ok.grab_focus()
 
     def __on_btn_ok_clicked(self, btn):
-        pass
+        """Save changes made to the selected surface form, save it and move on
+            to the next one."""
+        root = None
+        pos  = None
+
+        if self.current_sf.root_id != self.current_root.id:
+            self.current_sf.root_id = self.current_root.id
+            self.current_sf.user_id = self.config.options['user_id']
+
+        self.current_sf.status  = 'classified'
+        self.wordlist.refresh() # This will select the next word at the top of the word list
 
     def __on_btn_reject_clicked(self, btn):
         """Set the currently selected surface form's status to "rejected"."""
@@ -265,24 +432,42 @@ class EditArea(object):
         self.current_sf.status = 'rejected'
         self.gui.reload_database()
 
-    def __on_cmb_pos_changed(self, cmb_pos):
-        assert cmb_pos is self.cmb_pos
+    def __on_cmb_changed(self, combo, select_model):
+        """Handler for the "changed" event of a gtk.ComboBox.
 
-        # TODO: Handle root with different POS here.
+            @type  select_model: function
+            @param select_model: The model that should handle the selection of
+                the new model."""
+        iter = combo.get_active_iter()
 
-    def __on_cmb_root_changed(self, cmb_root):
-        assert cmb_root is self.cmb_root
+        if not iter is None:
+            model = combo.get_model().get_value(iter, COL_MODEL)
+            select_model(model)
 
-    def __on_match_selected(self, completion, store, iter, combo):
+    def __on_match_selected(self, completion, store, iter, combo, select_model):
+        """Handler for the "match-selected" event of ComboBoxEntries'
+            EntryCompletions.
+
+            See the PyGtk API documentation for the definition of the first 3
+            parameters. It should be obvious, though.
+            @type  combo: gtk.ComboBoxEntry
+            @param combo: The combo box for which a match was selected.
+            @type  select_model: function
+            @param select_model: The function to which the selected model
+                should be passed to handle its selection."""
         child_iter = store.convert_iter_to_child_iter(iter)
-        if combo is self.cmb_root:
-            self.cmb_pos.grab_focus()
-        else:
-            self.btn_reject.grab_focus()
-        combo.set_active_iter(child_iter)
-        combo.child.set_text( combo.get_model().get_value(child_iter, COL_TEXT) )
+        model = combo.get_model().get_value(child_iter, COL_MODEL)
+        select_model(model)
 
         return True
+
+    def __on_entry_activated(self, entry, text_handler):
+        """Handler for the "activated" event of a gtk.Entry.
+
+            It is used here for the child Entries of the ComboBoxEntries.
+            @type  text_handler: function
+            @param text_handler: The function that will handle the text entered."""
+        text_handler(entry.get_text())
 
     def __render_pos(self, layout, cell, store, iter):
         """Cell data function that renders a part-of-speech from it's model in
