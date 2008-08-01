4198acfc7c26a05743e831d27de6b2f466eab299
Walter Leibbrandt <walter@translate.org.za>
2008-08-01 14:15:02 +0000

    Changed dlg_first_run to dlg_dbload. This caused some errors on Windows.
    I wonder why we didn't have any problem with it under Linux... curious.
    
    svn path=/src/trunk/spelt/; revision=7949

--- spelt/gui/__init__.py
+++ spelt/gui/__init__.py
@@ -18,14 +18,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-from dlg_first_run import DlgFirstRun
-from dlg_source    import DlgSource
-from gui           import GUI
-from edit_area     import EditArea
-from wordlist      import WordList
+from dlg_dbload import DlgDBLoad
+from dlg_source import DlgSource
+from gui        import GUI
+from edit_area  import EditArea
+from wordlist   import WordList
 
 __all__ = [
-    'DlgFirstRun',
+    'DlgDBLoad',
     'DlgSource',
     'EditArea',
     'GUI',
